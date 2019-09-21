47f740cb8e29d68349d99782ada50eccf9c614f6
Derek Miranda <derek.miranda@ff0000.com>
2019-09-20 17:32:11 -0700

    Adds iris vars
    - updates README
    - redist

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # White Iris Animation Preset
 
 White iris reveals endframe
+
+## Variables
+
+- `irisDelay`: time to delay start of iris animation (in seconds)
+- `irisDuration`: how long iris expansion lasts
+- `irisColor`: color of iris screen
--- src/creative.js
+++ src/creative.js
@@ -3,29 +3,35 @@
  * See here for more details: https://github.com/ff0000-ad-tech/ad-canvas
  */
 var Creative = function() {
+  // percentage of iris animation that has to elapse before content underneath is completely visible
+  var irisOffscreenAnimPercent = 0.3;
   this.play = function() {
     console.log('Creative.play()');
 
-    var irisDelay = 0.5;
-    var irisTime = 3.0;
+    var irisDelay = Creative.irisDelay;
+    var irisDuration = Creative.irisDuration;
     var irisLen = Math.max(adParams.adWidth, adParams.adHeight);
-    View.endFrame.iris.tween.to(View.endFrame.iris.circle, irisTime, {
+    View.endFrame.iris.tween.to(View.endFrame.iris.circle, irisDuration, {
       delay: irisDelay,
       scale: irisLen * 0.05,
       ease: Power2.easeOut
     });
     View.endFrame.iris.tween.start();
 
-    var logoDelay = irisDelay + Creative.irisOffscreenAnimPercent * irisTime;
+    var logoDelay = irisDelay + irisOffscreenAnimPercent * irisDuration;
     TweenLite.delayedCall(logoDelay, function() {
       View.endFrame.netflixLogo.play();
     });
   };
 };
 
-// percentage of iris animation that has to elapse before content underneath is completely visible
-Creative.irisOffscreenAnimPercent = 0.3;
-Creative.usesCanvasIris = true;
+// time to delay start of iris animation (in seconds)
+Creative.irisDelay = 0.5;
+
+// how long iris expansion lasts
+Creative.irisDuration = 2;
+
+// color of iris screen
 Creative.irisColor = 'white';
 
 // how long zoom animates for (in seconds)
@@ -33,3 +39,6 @@ Creative.zoomDuration = 1.7;
 
 // how much to scale the keyart intro frame
 Creative.zoomAmount = 5;
+
+// indicates whether to use canvas-rendered iris
+Creative.usesCanvasIris = true;
